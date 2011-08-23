4f4529954b769bcb75150ee96f73faf1b43165b0
bonetblai <bonetblai@gmail.com>
2011-08-23 13:55:58 +0000

    Add options to kp_problem and classical_planner.

--- src/kp_problem.h
+++ src/kp_problem.h
@@ -2,6 +2,7 @@
 #define KP_PROBLEM_H
 
 #include "problem.h"
+#include "verbosity.h"
 
 class KP_Instance : public Instance {
   public:
@@ -9,21 +10,26 @@ class KP_Instance : public Instance {
     size_t n_sensor_actions_;
     size_t n_invariant_actions_;
     std::map<std::string, int> obs_rules_by_name_;
-    KP_Instance(const Instance &instance);
+    KP_Instance(const Instance &instance, const Verbosity::Mode &vmode = Verbosity::Mode());
     ~KP_Instance() { }
     size_t first_deductive_action() const { return n_standard_actions_ + n_sensor_actions_; }
     bool is_obs_rule(const std::string &name) const {
         return obs_rules_by_name_.find(name) != obs_rules_by_name_.end();
     }
     bool is_obs_rule(size_t a) const {
-        size_t lower = n_standard_actions_, upper = lower + n_sensor_actions_;
+        size_t lower = n_standard_actions_;
+        size_t upper = lower + n_sensor_actions_;
         return (a >= lower) && (a < upper);
     }
     bool is_static_rule(size_t a) const {
-        size_t lower = n_standard_actions_ + n_sensor_actions_, upper = lower + n_invariant_actions_;
+        size_t lower = n_standard_actions_ + n_sensor_actions_;
+        size_t upper = lower + n_invariant_actions_;
         return (a >= lower) && (a < upper);
     }
-    void apply_plan(const Plan &plan, const State &initial_state, State &final_state, std::vector<State> &support_vec) const;
+    void apply_plan(const Plan &plan,
+                    const State &initial_state,
+                    State &final_state,
+                    std::vector<State> &support_vec) const;
 };
 
 #endif
