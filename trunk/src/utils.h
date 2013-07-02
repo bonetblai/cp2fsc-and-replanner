620c1376b280fcb3c7a0a22674ede933fc7b99f8
bonetblai@gmail.com <bonetblai@gmail.com@bddcbf9f-a65d-19ce-779b-d87b6116cf92>
2013-07-02 13:37:05 +0000

    revised get_time_in_seconds

--- trunk/src/utils.h
+++ trunk/src/utils.h
@@ -6,19 +6,26 @@
 
 namespace Utils {
 
-inline float read_time_in_seconds() {
+inline float read_time_in_seconds(bool add_stime = false) {
     struct rusage r_usage;
     float time = 0;
+
     getrusage(RUSAGE_SELF, &r_usage);
     time += (float)r_usage.ru_utime.tv_sec +
-            (float)r_usage.ru_utime.tv_usec / (float)1000000 +
-            (float)r_usage.ru_stime.tv_sec +
-            (float)r_usage.ru_stime.tv_usec / (float)1000000;
+            (float)r_usage.ru_utime.tv_usec / (float)1000000;
+    if( add_stime ) {
+        time += (float)r_usage.ru_stime.tv_sec +
+                (float)r_usage.ru_stime.tv_usec / (float)1000000;
+    }
+
     getrusage(RUSAGE_CHILDREN, &r_usage);
     time += (float)r_usage.ru_utime.tv_sec +
-            (float)r_usage.ru_utime.tv_usec / (float)1000000 +
-            (float)r_usage.ru_stime.tv_sec +
-            (float)r_usage.ru_stime.tv_usec / (float)1000000;
+            (float)r_usage.ru_utime.tv_usec / (float)1000000;
+    if( add_stime ) {
+        time += (float)r_usage.ru_stime.tv_sec +
+                (float)r_usage.ru_stime.tv_usec / (float)1000000;
+    }
+
     return time;
 }
 
