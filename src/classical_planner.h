d7cb2ee6d790bd483e54fb85372d63031e06ee26
bonetblai <bonetblai@gmail.com>
2013-07-08 14:24:48 +0000

    Add optional path to tmp files

--- src/classical_planner.h
+++ src/classical_planner.h
@@ -13,6 +13,7 @@ class ClassicalPlanner {
     enum { SOLVED = 0, NO_SOLUTION = 1, ERROR = 2 };
   protected:
     std::string name_;
+    std::string tmpfile_path_;
     std::string planner_path_;
     std::string planner_name_;
     const Instance &instance_;
@@ -28,7 +29,7 @@ class ClassicalPlanner {
     mutable float total_time_;
     mutable size_t n_calls_;
   public:
-    ClassicalPlanner(const char *name, const char *planner_path, const char *planner_name, const Instance &instance);
+    ClassicalPlanner(const char *name, const char *tmpfile_path, const char *planner_path, const char *planner_name, const Instance &instance);
     virtual ~ClassicalPlanner() { }
     virtual int get_plan(const State &state, Instance::Plan &plan) const = 0;
     const char* name() const { return planner_name_.c_str(); }
@@ -42,8 +43,8 @@ class ClassicalPlanner {
 class FF_Planner : public ClassicalPlanner {
     mutable bool first_call_;
   public:
-    FF_Planner(const Instance &instance, const char *planner_path)
-      : ClassicalPlanner("FF", planner_path, "ff", instance), first_call_(true) { }
+    FF_Planner(const Instance &instance, const char *tmpfile_path, const char *planner_path)
+      : ClassicalPlanner("FF", tmpfile_path, planner_path, "ff", instance), first_call_(true) { }
     virtual ~FF_Planner();
     virtual int get_plan(const State &state, Instance::Plan &plan) const;
 };
@@ -58,7 +59,7 @@ class LAMA_Planner : public ClassicalPlanner {
     mutable std::vector<std::vector<int> > variables_;
     std::map<std::string, size_t> atom_map_;
   public:
-    LAMA_Planner(const Instance &instance, const char *planner_path);
+    LAMA_Planner(const Instance &instance, const char *tmpfile_path, const char *planner_path);
     virtual ~LAMA_Planner();
     virtual int get_plan(const State &state, Instance::Plan &plan) const;
     void patch_output_sas(std::fstream &iofs, const State &state) const;
@@ -70,16 +71,16 @@ class M_Planner : public ClassicalPlanner {
   protected:
     mutable bool first_call_;
   public:
-    M_Planner(const Instance &instance, const char *planner_path, const char *planner_name = "M")
-      : ClassicalPlanner(planner_name, planner_path, planner_name, instance), first_call_(true) { }
+    M_Planner(const Instance &instance, const char *tmpfile_path, const char *planner_path, const char *planner_name = "M")
+      : ClassicalPlanner(planner_name, tmpfile_path, planner_path, planner_name, instance), first_call_(true) { }
     virtual ~M_Planner();
     virtual int get_plan(const State &state, Instance::Plan &plan) const;
 };
 
 class MP_Planner : public M_Planner {
   public:
-    MP_Planner(const Instance &instance, const char *planner_path)
-      : M_Planner(instance, planner_path, "Mp") { }
+    MP_Planner(const Instance &instance, const char *tmpfile_path, const char *planner_path)
+      : M_Planner(instance, tmpfile_path, planner_path, "Mp") { }
     virtual ~MP_Planner();
 };
 
