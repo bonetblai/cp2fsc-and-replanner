5b91925d572f728bf8d2962e45cefa4b560d0310
bonetblai@gmail.com <bonetblai@gmail.com@bddcbf9f-a65d-19ce-779b-d87b6116cf92>
2013-07-11 14:12:02 +0000

    Fix bug with tmpfile_path

--- trunk/src/classical_planner.cc
+++ trunk/src/classical_planner.cc
@@ -22,27 +22,27 @@ ClassicalPlanner::ClassicalPlanner(const char *name,
     int pid = getpid();
 
     ostringstream sstr("");
-    if( planner_path_ != "" ) sstr << tmpfile_path_ << "/";
+    if( tmpfile_path_ != "" ) sstr << tmpfile_path_ << "/";
     sstr << "gen-d." << pid << ".pddl";
     domain_fn_ = strdup(sstr.str().c_str());
 
     sstr.str("");
-    if( planner_path_ != "" ) sstr << tmpfile_path_ << "/";
+    if( tmpfile_path_ != "" ) sstr << tmpfile_path_ << "/";
     sstr << "gen-p." << pid << ".pddl";
     problem_fn_ = strdup(sstr.str().c_str());
 
     sstr.str("");
-    if( planner_path_ != "" ) sstr << tmpfile_path_ << "/";
+    if( tmpfile_path_ != "" ) sstr << tmpfile_path_ << "/";
     sstr << planner_name_ << ".output." << pid;
     output_fn_ = strdup(sstr.str().c_str());
 
     sstr.str("");
-    if( planner_path_ != "" ) sstr << tmpfile_path_ << "/";
+    if( tmpfile_path_ != "" ) sstr << tmpfile_path_ << "/";
     sstr << planner_name_ << ".tmp." << pid;
     tmp_fn_ = strdup(sstr.str().c_str());
 
     sstr.str("");
-    if( planner_path_ != "" ) sstr << tmpfile_path_ << "/";
+    if( tmpfile_path_ != "" ) sstr << tmpfile_path_ << "/";
     sstr << planner_name_ << ".plan." << pid;
     plan_fn_ = strdup(sstr.str().c_str());
 }
