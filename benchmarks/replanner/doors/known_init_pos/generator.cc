866e4347768b2bc026a84589d80c07dc2e4ab704
bonetblai <bonetblai@gmail.com>
2013-12-23 22:02:35 +0000

    working

--- benchmarks/replanner/doors/known_init_pos/generator.cc
+++ benchmarks/replanner/doors/known_init_pos/generator.cc
@@ -8,18 +8,23 @@
 using namespace std;
 
 struct Instance {
-    int n_pos;
-    vector<int> doors;
-    Instance(int p) : n_pos(p), doors(p, 0) {
+    int n_pos_;
+    int n_instances_;
+    vector<vector<int> > instances_;
+    Instance(int p, int n) : n_pos_(p), n_instances_(n) {
         assert(p % 2 == 1);
-        for( int p = 1; p < n_pos; p += 2 ) {
-            doors[p] = lrand48() % n_pos;
+        instances_.reserve(n_instances_);
+        for( int i = 0; i < n_instances_; ++i ) {
+            vector<int> instance(n_pos_, 0);
+            for( int p = 1; p < n_pos_; p += 2 )
+                instance[p] = lrand48() % n_pos_;
+            instances_.push_back(instance);
         }
     }
 };
 
 void generate_problem(ostream &os, const Instance &ins) {
-    int n_pos = ins.n_pos;
+    int n_pos = ins.n_pos_, n_instances = ins.n_instances_;
 
     os << "(define (problem n" << n_pos << ")" << endl
        << "    (:domain colored-balls)" << endl;
@@ -58,11 +63,14 @@ void generate_problem(ostream &os, const Instance &ins) {
     os << "    )" << endl;
 
     // hidden situation
-    os << "    (:hidden";
-    for( int p = 1; p < n_pos; p += 2 ) {
-        os << " (door p" << p+1 << " p" << ins.doors[p]+1 << ")";
+    for( int i = 0; i < n_instances; ++i ) {
+        const vector<int> &instance = ins.instances_[i];
+        os << "    (:hidden";
+        for( int p = 1; p < n_pos; p += 2 ) {
+            os << " (door p" << p+1 << " p" << instance[p]+1 << ")";
+        }
+        os << ")" << endl;
     }
-    os << ")" << endl;
 
     // goal situation
     os << "    (:goal (at p" << n_pos << " p" << 1+(n_pos/2) << "))" << endl;
@@ -73,8 +81,8 @@ void generate_problem(ostream &os, const Instance &ins) {
 
 int main(int argc, const char **argv) {
     unsigned short seed[3];
-    if( argc != 3 ) {
-        cerr << "Usage: generator <seed> <n_pos:odd>" << endl;
+    if( argc != 4 ) {
+        cerr << "Usage: generator <seed> <n_pos:odd> <n_instances>" << endl;
         return -1;
     }
 
@@ -83,8 +91,9 @@ int main(int argc, const char **argv) {
     seed48(seed);
 
     int n_pos = atoi(*argv++);
+    int n_instances = atoi(*argv++);
     assert(n_pos % 2 == 1);
-    Instance ins(n_pos);
+    Instance ins(n_pos, n_instances);
     generate_problem(cout, ins);
     return 0;
 }
