9ef7212d00f518db59e803f180781a95887d2df1
bonetblai@gmail.com <bonetblai@gmail.com@bddcbf9f-a65d-19ce-779b-d87b6116cf92>
2013-12-23 21:41:59 +0000

    working

--- trunk/benchmarks/replanner/balls/uncolored/generator.cc
+++ trunk/benchmarks/replanner/balls/uncolored/generator.cc
@@ -8,20 +8,26 @@
 using namespace std;
 
 struct Instance {
-    int n_pos;
-    int n_balls;
-    vector<int> balls;
-    Instance(int p, int b) : n_pos(p), n_balls(b) {
+    int n_pos_;
+    int n_balls_;
+    int n_placements_;
+    vector<vector<int> > placements_;
+    Instance(int p, int b, int n) : n_pos_(p), n_balls_(b), n_placements_(n) {
         // place balls
-        for( int b = 0; b < n_balls; ++b ) {
-            int p = lrand48() % n_pos, q = lrand48() % n_pos;
-            balls.push_back(q*n_pos + p);
+        placements_.reserve(n);
+        for( int i = 0; i < n_placements_; ++i ) {
+            vector<int> placement(n_balls_, 0);
+            for( int b = 0; b < n_balls_; ++b ) {
+                int p = lrand48() % n_pos_, q = lrand48() % n_pos_;
+                placement[b] = q*n_pos_ + p;
+            }
+            placements_.push_back(placement);
         }
     }
 };
 
 void generate_problem(ostream &os, const Instance &ins) {
-    int n_pos = ins.n_pos, n_balls = ins.n_balls;
+    int n_pos = ins.n_pos_, n_balls = ins.n_balls_, n_placements = ins.n_placements_;
 
     os << "(define (problem n" << n_pos << "-b" << n_balls << ")" << endl
        << "    (:domain balls)" << endl;
@@ -61,12 +67,16 @@ void generate_problem(ostream &os, const Instance &ins) {
     os << "    )" << endl;
 
     // hidden situation
-    os << "    (:hidden";
-    for( int b = 0; b < n_balls; ++b ) {
-        int pos = ins.balls[b], p = pos % n_pos, q = pos / n_pos;
-        os << " (ball-at b" << b+1 << " p" << p+1 << " p" << q+1 << ")";
+    for( int i = 0; i < n_placements; ++i ) {
+        const vector<int> &placement = ins.placements_[i];
+        assert(placement.size() == n_balls);
+        os << "    (:hidden";
+        for( int b = 0; b < n_balls; ++b ) {
+            int pos = placement[b], p = pos % n_pos, q = pos / n_pos;
+            os << " (ball-at b" << b+1 << " p" << p+1 << " p" << q+1 << ")";
+        }
+        os << ")" << endl;
     }
-    os << ")" << endl;
 
     // goal situation
     os << "    (:goal (and";
@@ -80,8 +90,8 @@ void generate_problem(ostream &os, const Instance &ins) {
 
 int main(int argc, const char **argv) {
     unsigned short seed[3];
-    if( argc != 4 ) {
-        cerr << "Usage: generator <seed> <n_pos> <n-balls>" << endl;
+    if( argc != 5 ) {
+        cerr << "Usage: generator <seed> <n_pos> <n-balls> <n-placements>" << endl;
         return -1;
     }
 
@@ -91,7 +101,8 @@ int main(int argc, const char **argv) {
 
     int n_pos = atoi(*argv++);
     int n_balls = atoi(*argv++);
-    Instance ins(n_pos, n_balls);
+    int n_placements = atoi(*argv++);
+    Instance ins(n_pos, n_balls, n_placements);
     generate_problem(cout, ins);
     return 0;
 }
