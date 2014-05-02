4c1cc8d9affa878f1f96124e78709bc01dfea12c
miquel.ramirez <miquel.ramirez@gmail.com>
2014-05-02 03:13:39 +0000

    Work in Progress: Integration with lwaptk
    * bfs_f_planner.cc: removed useless messages
    
    * New: SIW Planner (siw_planner.cc,.h)
    
    * k-replanner.cc: planner option "siw" is now handled

---
+++ src/siw_planner.h
@@ -0,0 +1,24 @@
+#ifndef __SIW_PLANNER__
+#define __SIW_PLANNER__
+
+#include "lwaptk_planner.h"
+
+class SIW_Planner : public Lwaptk_Planner {
+
+public:
+
+	SIW_Planner( const KP_Instance& instance, const char* tmpfile_path );
+	virtual ~SIW_Planner();
+
+	virtual int classical_planner(const State &state, Instance::Plan &raw_plan) const;
+
+	void	set_classical_width_initial_bound( unsigned v ) { m_iw_bound = v; }
+
+protected:
+	
+	// MRJ: Maximum width to be considered before aborting (default is 4)
+	unsigned 	m_iw_bound;
+
+};
+
+#endif // siw_planner.h
