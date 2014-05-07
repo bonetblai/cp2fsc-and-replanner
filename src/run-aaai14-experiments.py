b64a313e96646818a2dd0e14a6aba8d189d0f103
miquel.ramirez <miquel.ramirez@gmail.com>
2014-05-07 23:24:01 +0000

    Adjustments made to the scripts overseeing experimental evaluation of planners

--- src/run-aaai14-experiments.py
+++ src/run-aaai14-experiments.py
@@ -3,7 +3,7 @@ import os
 import glob
 
 from experimenter import benchmark
-from experimenter.lw1_configs import BFS_f, SIW
+from experimenter.lw1_configs import BFS_f, DFS_RPIW, FF
 
 
 def main() :
@@ -36,34 +36,41 @@ def main() :
 				['wumpus', 'p25d']
 			]
 
-	planners = [ SIW(), BFS_f() ]
+	planners = [ FF(), DFS_RPIW(), BFS_f() ]
 
 	benchmarks_base_dir = '../benchmarks/lw1'
-	max_time = 7200 # 4 hours
+	max_time = 1800 # half an hour
 	max_mem = 4096 # 4 Gigabytes
+	num_H = 10
+	seed = 8371
 
-	for planner in planners :
-		for domain, instance in aaai14_benchmark :
-			instance_base_dir = os.path.join( benchmarks_base_dir, domain )
-			instance_base_dir = os.path.join( instance_base_dir, 'mvv' )
-			instance_base_dir = os.path.join( instance_base_dir, instance )
-			
-			pddl_inputs = glob.glob( os.path.join(instance_base_dir, '*.pddl') )
+	for domain, instance in aaai14_benchmark :
+		instance_base_dir = os.path.join( benchmarks_base_dir, domain )
+		instance_base_dir = os.path.join( instance_base_dir, 'mvv' )
+		instance_base_dir = os.path.join( instance_base_dir, instance )
+		
+		pddl_inputs = glob.glob( os.path.join(instance_base_dir, '*.pddl') )
 
-			pddl_domain = None
-			pddl_problem = None
-			pddl_hidden = None
+		pddl_domain = None
+		pddl_problem = None
+		pddl_hidden = None
 
-			for filename in pddl_inputs :
-				if os.path.split(filename)[-1][0] == 'd' :
-					pddl_domain = filename
-				if os.path.split(filename)[-1][0] == 'p' :
-					pddl_problem = filename
-				if os.path.split(filename)[-1][0] == 'h' :
-					pddl_hidden = filename
-				
+		for filename in pddl_inputs :
+			if os.path.split(filename)[-1][0] == 'd' :
+				pddl_domain = filename
+			if os.path.split(filename)[-1][0] == 'p' :
+				pddl_problem = filename
+			if 'new-p' in os.path.split(filename)[-1] :
+				pddl_problem = filename
+			if os.path.split(filename)[-1][0] == 'h' :
+				pddl_hidden = filename
 
-			command = './lw1-lwaptk %s %s %s %s'%( planner.get_flags(), pddl_domain, pddl_problem, pddl_hidden )
+		for planner in planners :
+						
+			# Exhaustive	
+			# flags = planner.get_flags()
+			flags = planner.get_random_flags( num_H, seed )
+			command = './lw1-lwaptk %s %s %s %s'%( flags, pddl_domain, pddl_problem, pddl_hidden )
 			log_filename = '%s-%s-%s.log'%(planner.planner, domain, instance)
 			log = benchmark.Log( log_filename )
 
