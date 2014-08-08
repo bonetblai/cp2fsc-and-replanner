838037c57b3b937738cb0fb79a4b3ab428277a09
christian.muise@gmail.com <christian.muise@gmail.com@bddcbf9f-a65d-19ce-779b-d87b6116cf92>
2014-08-08 02:44:29 +0000

    Put in more examples for the testing script to test various translations.

--- branches/christian/src/runme.sh
+++ branches/christian/src/runme.sh
@@ -1,1 +1,9 @@
-./k-replanner --print-domain-only --options=kp:print:preprocessed --keep-intermediate-files --max-time 10 ../benchmarks/replanner/CLG/wumpus/wumpus05/d.pddl ../benchmarks/replanner/CLG/wumpus/wumpus05/p.pddl
+# Standard
+#./k-replanner --print-domain-only --options=kp:print:preprocessed --keep-intermediate-files --max-time 10 ../benchmarks/replanner/doors/known_init_pos/domain.pddl ../benchmarks/replanner/doors/known_init_pos/problems/n05.pddl
+
+# CLG
+#./k-replanner --print-domain-only --options=kp:print:preprocessed --keep-intermediate-files --max-time 10 ../benchmarks/replanner/CLG/wumpus/wumpus05/d.pddl ../benchmarks/replanner/CLG/wumpus/wumpus05/p.pddl
+
+# MVV2
+# ./k-replanner --print-domain-only --options=kp:print:preprocessed --keep-intermediate-files --max-time 10 ../benchmarks/replanner/CLG/wumpus/multivalued-variables/diag05/d.pddl ../benchmarks/replanner/CLG/wumpus/multivalued-variables/diag05/p.pddl
+./k-replanner --print-domain-only --options=kp:print:preprocessed --keep-intermediate-files --max-time 10 ../benchmarks/replanner/CLG/doors/multivalued-variables/d.pddl ../benchmarks/replanner/CLG/doors/multivalued-variables/p5.pddl
