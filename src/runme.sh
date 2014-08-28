329cb716712181f59b87767cc0f2f47bbcc0c343
christian.muise <christian.muise@gmail.com>
2014-08-28 01:28:04 +0000

    Extended the testing script a bit to squelch output.

--- src/runme.sh
+++ src/runme.sh
@@ -4,13 +4,16 @@ USAGE="Usage: ./runme.sh <standard/clg/mvv2> <model/raw/preprocessed>"
 
 if [ "model" = "$2" ];
 then
-    OPT="mvv:print:compiled-sensing-models"
+    OPT=" --options=mvv:print:compiled-sensing-models"
 elif [ "raw" = "$2" ];
 then
-    OPT="problem:print:raw"
+    OPT=" --options=problem:print:raw"
 elif [ "preprocessed" = "$2" ];
 then
-    OPT="kp:print:preprocessed"
+    OPT=" --options=kp:print:preprocessed"
+elif [ "none" = "$2" ];
+then
+    OPT = ""
 else
     echo
     echo $USAGE
@@ -20,13 +23,14 @@ fi
 
 if [ "standard" = "$1" ];
 then
-    ./k-replanner --print-domain-only --options=$OPT --keep-intermediate-files --max-time 10 ../benchmarks/replanner/doors/known_init_pos/domain.pddl ../benchmarks/replanner/doors/known_init_pos/problems/n05.pddl
+    ./k-replanner --print-domain-only$OPT --keep-intermediate-files --max-time 10 ../benchmarks/replanner/doors/known_init_pos/domain.pddl ../benchmarks/replanner/doors/known_init_pos/problems/n05.pddl
 elif [ "clg" = "$1" ];
 then
-    ./k-replanner --print-domain-only --options=$OPT --keep-intermediate-files --max-time 10 ../benchmarks/replanner/CLG/wumpus/wumpus05/d.pddl ../benchmarks/replanner/CLG/wumpus/wumpus05/p.pddl
+    ./k-replanner --print-domain-only$OPT --keep-intermediate-files --max-time 10 ../benchmarks/replanner/CLG/wumpus/wumpus05/d.pddl ../benchmarks/replanner/CLG/wumpus/wumpus05/p.pddl
 elif [ "mvv2" = "$1" ];
 then
-    ./k-replanner --print-domain-only --options=$OPT --keep-intermediate-files --max-time 10 ../benchmarks/replanner/CLG/wumpus/multivalued-variables/diag05/d.pddl ../benchmarks/replanner/CLG/wumpus/multivalued-variables/diag05/p.pddl
+    ./k-replanner --print-domain-only$OPT --keep-intermediate-files --max-time 10 ../benchmarks/replanner/CLG/wumpus/multivalued-variables/diag05/d.pddl ../benchmarks/replanner/CLG/wumpus/multivalued-variables/diag05/p.pddl
+    #./k-replanner --print-domain-only$OPT --keep-intermediate-files --max-time 10 ../benchmarks/replanner/CLG/doors/multivalued-variables/new-d.pddl ../benchmarks/replanner/CLG/doors/multivalued-variables/new-p5.pddl
 else
     echo
     echo $USAGE
