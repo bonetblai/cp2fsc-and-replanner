4b0c65e2d71daa64930d2f81c7e289a0c89c308f
gabrielformica93@gmail.com <gabrielformica93@gmail.com@bddcbf9f-a65d-19ce-779b-d87b6116cf92>
2015-04-15 19:39:16 +0000

    implemented watched literals

--- branches/pure-lw1/src/Inference.cc
+++ branches/pure-lw1/src/Inference.cc
@@ -1,26 +1,23 @@
+#include <set>
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
 #include "Inference.h"
+using namespace std;
 
-void Inference::Propositional::Clause::push_back(int e) {
-    insert(e); // See comment in lw1_solver.cc
-}
+typedef Inference::Propositional::CNF::const_iterator const_vec_set_it;
+typedef vector< vector<int> >::iterator vec_vec_it;
+typedef set<int>::iterator set_it;
+typedef vector<int>::iterator vec_it;
 
-int Inference::Propositional::CNF::calculate_max() {
-   int imax = 0;
-    for (CNF::const_iterator it = begin(); it != end(); it++) {
-        int L1 =  abs(*(it->begin()));
-        int L2 =  abs(*(it->rbegin()));
-        imax = max(L1, max(L2, imax));
-    }
-    return imax;
-}
-
-void Inference::Propositional::UnitPropagation::reduce(const CNF &a, CNF &b) {
+void Inference::Propositional::DPLL::solve(const CNF &a, CNF &b) {
     b = CNF(a);
-
     bool change = true;
     while (change) {
         change = false;
-        sort(b.begin(), b.end(), this->compare);
+        sort(b.begin(), b.end());
         for (CNF::iterator it = b.begin(); it != b.end(); it++) {
             if (it->size() == 1) {
                 int L = *(it->begin());
@@ -36,6 +33,84 @@ void Inference::Propositional::UnitPropagation::reduce(const CNF &a, CNF &b) {
                     }
                 }
             }
+        }
+    }
+}
+
+void Inference::Propositional::WatchedLiterals::InvertedIndex(const CNF &a) {
+    inverted_index.assign(imax + 1, vector<int>());
+    int clause = 0;
+    for (const_vec_set_it it = a.begin(); it != a.end(); it++) {
+        for (set_it s_it = it->begin(); s_it != it->end(); s_it++) {
+            inverted_index[ abs(*s_it) ].push_back(clause);
+        }
+        clause++;
+    }
+}
+
+void Inference::Propositional::WatchedLiterals::initialize(const CNF &a) {
+    imax = a.calculate_max();
+    satisfied.assign(a.size(), false); 
+    propagated.assign(imax + 1, false); //anything has been propagated
+    InvertedIndex(a);
+
+    //watch first (0) and last (size - 1) element
+    for (int i = 0; i < a.size(); i++)
+        watched.push_back(pair<set_it, set_it>(a[i].begin(), 
+                                               --a[i].end()));
+};
+
+
+void Inference::Propositional::WatchedLiterals::solve(const CNF &a, 
+                                                      vector<int> &assigned) { 
+    initialize(a);
+    assigned.assign(imax + 1, -1);      //initially, every proposition is unassigned
+    for (const_vec_set_it it = a.begin(); it != a.end(); it++) {
+        int p = *(it->begin());
+        if ( (it->size() == 1) && !propagated[ abs(p) ] ) {
+            assigned[ abs(p) ] = p > 0 ? 1 : 0;
+            propagate(a, assigned, abs(p));
+            propagated[ abs(p) ] = true;
+        }
+    }
+}
+
+set_it Inference::Propositional::WatchedLiterals::replace(const CNF &a,
+                                                          vector<int> &assigned,
+                                                          int clause) {
+    set_it w1 = a[clause].begin(), w2 = watched[clause].second;
+    for (; w1 != a[clause].end(); w1++) 
+        if (assigned[ *w1 ] == -1 && w1 != w2) break;
+    return w1;
+}
+
+void Inference::Propositional::WatchedLiterals::propagate(const CNF &a, 
+                                                          vector<int> &assigned,
+                                                          int prop) {
+    assert(prop > 0);
+    vector<int> cp = vector<int>(); //index of clauses where not p exist
+    int value = assigned[prop] ? prop : -prop;
+    //adding clauses with negative watched literals associated with p
+    for (int i = 0; i < inverted_index[prop].size(); i++) {
+        int clause = inverted_index[prop][i];
+        set_it w1 = watched[clause].first, w2 = watched[clause].second;
+        bool is_watched = value == -1 * (* w1) || value == -1 * (* w2);
+
+        if (is_watched && !satisfied[clause])
+            cp.push_back(clause);  
+    }
+    
+    for (int i = 0; i < cp.size(); i++) {
+        int clause = cp[i];
+        if (*(watched[clause].first) != -1 * value) 
+            swap(watched[clause].first, watched[clause].second);
+
+        set_it w1 = replace(a, assigned, clause), w2 = watched[clause].second;
+        //Propagate
+        if (w1 == a[clause].end() && assigned[*w2] == -1) {
+            int p1 = *w2; 
+            assigned[ abs(p1) ] = p1 > 0 ? 1 : 0;
+            propagate(a, assigned, p1);
         }
     }
 }
--- branches/pure-lw1/src/Inference.h
+++ branches/pure-lw1/src/Inference.h
@@ -1,44 +1,78 @@
-#include <algorithm>
-#include <iostream>
-#include <cstdlib>
-#include <vector>
-#include <queue>
 #include <set>
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
 #ifndef INFERENCE_H
 #define INFERENCE_H
-#define UP
 
-using namespace std;
-
-// PROVISIONAL: Create classes necessary for expanding the Inference
-// and Propositional choices. These tangled namespaces are provisional.
 namespace Inference {
     namespace Propositional {
+        //iterators typedef
+        typedef vector< vector<int> >::iterator vec_vec_it;
+        typedef set<int>::iterator set_it;
+        typedef vector<int>::iterator vec_it;
+
         class Clause : public set<int> {
         public:
             void push_back(int e); // See comment at lw1_solver.cc
+            struct compare {   // Overloading < 
+                bool operator() (Clause a, Clause b) { return a.size() < b.size(); }
+            } compare;
         };
 
         class CNF : public vector<Clause> {
+            typedef CNF::const_iterator const_vec_set_it;
         public:
             CNF() : vector<Clause>() { };
             CNF(const CNF& a) : vector<Clause>(a.begin(), a.end()) { };
-            int calculate_max();
+            int calculate_max() const {
+                int imax = 0;
+                for (const_vec_set_it it = begin(); it != end(); it++) {
+                    int L1 =  abs(*(it->begin()));
+                    int L2 =  abs(*(it->rbegin()));
+                    imax = max(L1, max(L2, imax));
+                }
+                return imax;
+            }
         };
 
         class UnitPropagation {
         public:
-            struct compare {
-                bool operator() (Clause a, Clause b) { return a.size() < b.size(); }
-            } compare;
+            // PROVISIONAL: This method should make the class abstract,
+            // in order to use better and implementations of reduce under
+            // classic UP transparently.
+            // virtual void solve() = 0;
+        };
 
-            UnitPropagation() { };
+        class DPLL: public UnitPropagation {
+            public:
+            DPLL() { };
+            void solve(const CNF &a, CNF &b);
+        };
+
+
+        class WatchedLiterals : public UnitPropagation {
+        private:
+            vector<bool> propagated;
+            vector<bool> satisfied;  //satisfied clauses
+            vector< vector<int> > inverted_index;
+            vector< pair<set_it, set_it> > watched;
+            int imax;
+            set_it replace(const CNF &a, vector<int> &assigned, int clause); 
+
+            void InvertedIndex(const CNF &a);
+            virtual void propagate(const CNF &a, vector<int> &assigned, int p);
+        public:
+            WatchedLiterals() { };
+            void initialize(const CNF &a); 
             // PROVISIONAL: This method should make the class abstract,
             // in order to use better and implementations of reduce under
             // classic UP transparently.
             // virtual void reduce() = 0;
-            virtual void reduce(const CNF& a, CNF& b);
+            void solve(const CNF &a, vector<int> &assigned);
         };
     } // Propositional namespace
 } // Inference namespace
