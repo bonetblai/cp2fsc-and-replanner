79f15ca41ad1d919f7fc49876bf425b785f169b1
Blai Bonet <blaibonet@imac27.local>
2016-10-08 14:31:45 -0400

    Add copyright header to source files

--- src/base.cc
+++ src/base.cc
@@ -1,3 +1,21 @@
+/*
+ *  Copyright (C) 2011 - <date> Blai Bonet, Universidad Simon Bolivar
+ * 
+ *  Permission is hereby granted to distribute this software for
+ *  non-commercial research purposes, provided that this copyright
+ *  notice is included with any such distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ *  EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
+ *  SOFTWARE IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU
+ *  ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+ *
+ *  Blai Bonet, bonet@ldc.usb.ve, bonetblai@gmail.com
+ *
+ */
+
 #include <cassert>
 #include <iomanip>
 #include <stdlib.h>
