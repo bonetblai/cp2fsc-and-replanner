6a27a4cce68d7e2eea7c9b19e312f52c1e5613ae
Blai Bonet <bonetblai@gmail.com>
2018-07-22 06:13:47 -0400

    Add results.hpc-as

---
+++ results.hpc-as/stats.R
@@ -0,0 +1,16 @@
+options(width=200)
+library(plyr)
+data = read.csv("data.csv", header=T)
+good = data[data$plan.size != -1, ]
+ddply(good, .(family,domain.file,problem,planner), summarise,
+      instance.time.N = length(instance.time),
+      planner.calls.mean = mean(planner.calls),
+      plan.size.mean = mean(plan.size),
+      instance.time.mean = mean(instance.time),
+      prep.time.mean = mean(prep.time + planner.time - planner.search.time),
+      exec.time.mean = mean(instance.time - planner.time + planner.search.time)
+      #time.per.planner.call.mean = instance.time.mean / planner.calls.mean
+      #instance.time.sd = sd(instance.time),
+      #instance.time.se = instance.time.sd / sqrt(instance.time.N)
+)
+
